fb22dbc7aead7e068fb6b86e63a4bf7554b283e1
Takuo-IMBE <imbe@amoad.com>
2013-11-25 20:52:22 +0900

    created a bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+DIR="${HOME}/.dotfiles"
+
+if [ ! -d ${DIR} ]; then
+  git clone git@github.com:im6e/dotfiles.git ${DIR}
+fi
+
+pushd ${DIR}
+
+git submodule update --init
+
+for f in `ls -a`
+do
+  [ $f = "." ] && continue
+  [ $f = ".." ] && continue
+  [ $f = "README.md" ] && continue
+  [ $f = ".git" ] && continue
+  [ $f = ".gitignore" ] && continue
+  [ $f = ".gitmodules" ] && continue
+  [ $f = ".solarized" ] && continue
+  [ $f = "bootstrap.sh" ] && continue
+  ln -snf ${DIR}/$f ~/
+done
+
+vim -c ':NeoBundleInstall!' -c ':q!' -c ':q!'
+
+popd
